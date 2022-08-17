7f0ce396a1507bd379996eb19fe9a6ad5c71bccc
ESHAN99 <56473205+ESHAN99@users.noreply.github.com>
2022-08-17 23:38:24 +0530

    Add files via upload

---
+++ website_blocker.py
@@ -0,0 +1,29 @@
+from flask import redirect
+import time
+from datetime import datetime as dt
+
+hosts_temp = "hosts"
+hosts_path=r"C:\Windows\System32\drivers\etc\hosts"
+redirect="127.0.0.1"
+website_list = ["www.facebook.com", "facebook.com", "www.flipkart.com", "flipkart.com"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 10) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 12):
+        print("Working Hours....")
+        with open(hosts_path,'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write("\n" + redirect + " " + website + "\n")
+    else:
+        with open(hosts_path, 'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+                file.truncate()
+        print("Fun Hours....")
+    time.sleep(5)
